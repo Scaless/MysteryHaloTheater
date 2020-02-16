2566f6d9ca0bda9826fcb32fba9e62d631170435
Scales <scales@scal.es>
2020-02-16 11:06:43 -0500

    Starting to parse and display theater file data

--- MysteryHaloTheater/MysteryHaloTheater/MainWindow.xaml.cs
+++ MysteryHaloTheater/MysteryHaloTheater/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MysteryHaloTheater
 {
@@ -20,9 +21,74 @@ namespace MysteryHaloTheater
     /// </summary>
     public partial class MainWindow : Window
     {
+        ReachTheaterFile WorkingFile;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_LoadTheaterFile_Click(object sender, RoutedEventArgs e)
+        {
+            WorkingFile = null;
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"Low\MCC\Temporary\UserContent\HaloReach\Movie";
+            dialog.Filter = "Reach Theater Files (*.mov)|*.mov";
+            dialog.RestoreDirectory = true;
+
+            bool? result = dialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                if (File.Exists(dialog.FileName))
+                {
+                    WorkingFile = new ReachTheaterFile(dialog.FileName);
+                }
+            }
+
+            ReloadFileDisplay();
+        }
+
+        private void ClearDisplay()
+        {
+            lv_TheaterData.Items.Clear();
+            lv_Players.Items.Clear();
+
+            lbl_AuthorA.Content = WorkingFile?.CreatorNameA ?? "";
+            lbl_MissionName.Content = WorkingFile?.MissionName ?? "";
+            lbl_MissionDescription.Content = WorkingFile?.MissionFullDescription ?? "";
+        }
+
+        private void ReloadFileDisplay()
+        {
+            ClearDisplay();
+            
+            if (WorkingFile == null)
+            {
+                return;
+            }
+
+            foreach (var segment in WorkingFile.TheaterSegments)
+            {
+                StringBuilder hexBytes = new StringBuilder(segment.SegmentSize * 2);
+                for (int i = 7; i < segment.SegmentSize; i++)
+                {
+                    hexBytes.AppendFormat("{0:X}", segment.Data[i]);
+                }
+
+                lv_TheaterData.Items.Add(new {
+                    Size = segment.SegmentSize,
+                    EOF = segment.EOFFlag,
+                    Tick = segment.Tick,
+                    Data = hexBytes.ToString()
+                });
+            }
+
+            foreach(var player in WorkingFile.Players)
+            {
+                lv_Players.Items.Add(player);
+            }
+
         }
     }
 }
---
+++ MysteryHaloTheater/MysteryHaloTheater/ReachTheaterFile.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace MysteryHaloTheater
+{
+    class ReachTheaterFile
+    {
+        public string CreatorNameA; // 20 Bytes, ASCII
+        public string CreatorNameB; // 20 Bytes, ASCII
+        public string MissionName; // 256 Bytes, UTF16
+        public string MissionFullDescription; // 256 Bytes, UTF16
+
+        public Player[] Players = new Player[4]; 
+
+        public List<Segment> TheaterSegments = new List<Segment>();
+
+        public ReachTheaterFile(string TheaterFilePath)
+        {
+            LoadFile(TheaterFilePath);
+        }
+
+        private void LoadFile(string TheaterFilePath)
+        {
+            byte[] RawData = File.ReadAllBytes(TheaterFilePath);
+
+            // Basic info
+            CreatorNameA = System.Text.Encoding.ASCII.GetString(RawData, 0x88, 20);
+            CreatorNameB = System.Text.Encoding.ASCII.GetString(RawData, 0xAC, 20);
+            MissionName = System.Text.Encoding.Unicode.GetString(RawData, 0xC0, 256);
+            MissionFullDescription = System.Text.Encoding.Unicode.GetString(RawData, 0x1C0, 256);
+
+            Players[0].Name = System.Text.Encoding.Unicode.GetString(RawData, 0x1E228, 32);
+            Players[0].Tag = System.Text.Encoding.Unicode.GetString(RawData, 0x1E26C, 6);
+
+            Players[1].Name = System.Text.Encoding.Unicode.GetString(RawData, 0x1E330, 32);
+            Players[1].Tag = System.Text.Encoding.Unicode.GetString(RawData, 0x1E374, 6);
+
+            Players[2].Name = System.Text.Encoding.Unicode.GetString(RawData, 0x1E438, 32);
+            Players[2].Tag = System.Text.Encoding.Unicode.GetString(RawData, 0x1E47C, 6);
+
+            Players[3].Name = System.Text.Encoding.Unicode.GetString(RawData, 0x1E540, 32);
+            Players[3].Tag = System.Text.Encoding.Unicode.GetString(RawData, 0x1E584, 6);
+
+            int TagStartIndex = 0x1F760;
+            int SegmentStartIndex = 0;
+
+            // Get the index for the segment start
+            for (int i = TagStartIndex; ; i++)
+            {
+                if (i > RawData.Length - 2)
+                {
+                    break;
+                }
+
+                if(RawData[i] == 0x2B && RawData[i+1] == 0x80 && RawData[i+2] == 00)
+                {
+                    SegmentStartIndex = i + 3;
+                    break;
+                }
+            }
+
+            // Error: Failed to find segment start
+            if (SegmentStartIndex == 0)
+            {
+                return;
+            }
+
+            int CurrentSegmentIndex = SegmentStartIndex;
+            while (true)
+            {
+                byte SegmentSize = RawData[CurrentSegmentIndex + 3];
+                int SegmentDataIndex = CurrentSegmentIndex + 4;
+
+                int EOF = BitConverter.ToInt32(RawData, SegmentDataIndex);
+                
+
+                Segment seg = new Segment();
+                seg.SegmentSize = SegmentSize;
+
+                seg.EOFFlag = "";
+                if(RawData[SegmentDataIndex] != 0)
+                {
+                    seg.EOFFlag = System.Text.Encoding.ASCII.GetString(RawData, SegmentDataIndex, 3);
+                }
+
+                byte[] tickData = new byte[4];
+                Array.Copy(RawData, SegmentDataIndex + 3, tickData, 0, 4);
+                Array.Reverse(tickData);
+                seg.Tick = BitConverter.ToInt32(tickData, 0);
+
+                seg.Data = new List<byte>();
+                for (int i = 0; i < SegmentSize; i++)
+                {
+                    seg.Data.Add(RawData[SegmentDataIndex + i]);
+                }
+
+                TheaterSegments.Add(seg);
+
+                if (seg.EOFFlag == "eof") // "eof "
+                {
+                    break;
+                }
+
+                // Move to the next segment
+                CurrentSegmentIndex = SegmentDataIndex + SegmentSize;
+            }
+
+        }
+    }
+
+    struct Segment
+    {
+        public byte SegmentSize { get; set; }
+        public List<byte> Data { get; set; }
+        public string EOFFlag { get; set; }
+        public int Tick { get; set; }
+    }
+
+    struct Player
+    {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+    }
+
+}
